8dae483541b0849272ede643801e354ffe13be50
Agnes Corpuz <catherine_agnes.corpuz@genesys.om>
2023-10-17 15:23:46 +0800

    Add file_content_hash

--- blueprint/terraform/main.tf
+++ blueprint/terraform/main.tf
@@ -2,12 +2,9 @@ terraform {
   required_providers {
     genesyscloud = {
      source = "mypurecloud/genesyscloud"
+     version= "1.23.0"
     }
   }
-}
-
-provider "genesyscloud" {
-  sdk_debug = false
 }
 
 data "genesyscloud_user" "callback_agent" {
@@ -56,6 +53,7 @@ resource "genesyscloud_flow" "sms_eventrigger_flow" {
     module.callback_sms_dataaction
   ]  
   filepath = "${path.module}/architect/callback_sms_eventrigger_flow.yaml.tftpl"
+  file_content_hash = filesha256("${path.module}/architect/callback_sms_eventrigger_flow.yaml.tftpl")
   substitutions = {
     callback_originating_sms_phonenumber            = var.callback_originating_sms_phonenumber
     callback_phonenumber                            = var.callback_phonenumber
